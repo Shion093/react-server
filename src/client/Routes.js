e3f2af302e8f95196ae3cd5f441be3479d39c526
Alex Varela <shion@Alexs-MacBook-Pro.local>
2017-10-30 20:45:06 -0600

    new component

--- src/client/Routes.js
+++ src/client/Routes.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 import Home from './components/Home';
+import Users from './components/Users';
 
 export default () => {
   return (
     <div>
       <Route exact path={'/'} component={Home}/>
-      <Route exact path={'/users'} component={Home}/>
+      <Route exact path={'/users'} component={Users}/>
     </div>
   )
 }
---
+++ src/client/components/Users.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <div>
+        Users Home
+      </div>
+      <button onClick={() => console.log('hi there2')}>User button</button>
+    </div>
+  )
+};
+
+export default Home;
