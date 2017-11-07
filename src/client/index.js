fc9b318e1d3224c91b340affb7a479de5ae1e483
Alex Varela <shion@Alexs-MacBook-Pro.local>
2017-11-06 21:41:33 -0600

    webpack

--- src/client/index.js
+++ src/client/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import Routes from './Routes';
 
+import 'semantic-ui-css/semantic.min.css';
+
 ReactDOM.hydrate(
   <BrowserRouter>
     <Routes />
--- webpack.base.js
+++ webpack.base.js
@@ -7,13 +7,32 @@ module.exports = {
         loader  : 'babel-loader',
         exclude : /node_modules/,
         options : {
+          plugins : [
+            'transform-decorators-legacy',
+            'transform-class-properties'
+          ],
           presets : [
+            'es2015',
             'react',
             'stage-0',
-            ['env', { targets : { browsers : ['last 2 versions'] } }]
+            ['env', {targets : {browsers : ['last 2 versions']}}]
           ]
         }
-      }
+      },
+      {
+        test: /\.css$/,
+        loader: [ 'style-loader', 'css-loader' ]
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {}
+          }
+        ]
+      },
+      { test: /\.(png|woff|woff2|eot|ttf|svg)$/, loader: 'url-loader?limit=100000' },
     ]
   }
 };
